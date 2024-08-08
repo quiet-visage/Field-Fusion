5fdc399065898e787e26eca660d343489b9c237b
quiet-visage <0xffigor@gmail.com>
2024-08-08 20:00:06 -0300

    Demonstrate part of the new api

--- demo/demo.c
+++ demo/demo.c
@@ -11,6 +11,8 @@
 static const int kwindow_width = 1366;
 static const int kwindow_height = 768;
 static const char *kwin_title = "msdf demo";
+#define LOREM "Lorem ipsum odor amet, consectetuer adipiscing elit."
+
 
 static GLFWwindow *window;
 void init_gl_ctx() {
@@ -36,31 +38,40 @@ int main() {
     init_gl_ctx();
     ff_initialize("440");
 
-    FF_Glyph_Vec glyphs = ff_glyph_vec_create();
-    int dest[16];
-    ff_utf8_to_utf32(dest, "    hello world", 15);
-    char dest1[16];
-    ff_utf32_to_utf8(dest1, dest, 15);
-    ff_glyphs_vec_prealloc(&glyphs, 16);
-
     FF_Typo typo = {0, 16.f, 0xffffffff};
     FF_Style style = ff_style_create();
-    ff_print_utf32(glyphs.data, &glyphs.len, dest, 15, 100, 200,
-                   style);
-
     float projection[4][4];
     ff_get_ortho_projection(0, kwindow_width, kwindow_height, 0,
                             -1.0f, 1.0f, projection);
 
     for (; !glfwWindowShouldClose(window);) {
         glClear(GL_COLOR_BUFFER_BIT);
-        ff_draw(glyphs.data, glyphs.len, (float *)projection, style);
-        ff_draw_str32(L"Hello", 5, 300, 300, (float *)projection,
-                      style);
+
+        float y = 0.;
+        float orig_typo_size = style.typo.size;
+        for (size_t i = 0; i < 10; ++i) {
+            ff_draw_str8(LOREM, sizeof(LOREM) - 1, 10, y,
+                         (float *)projection, style);
+            style.typo.size += 2.;
+            y += style.typo.size;
+        }
+        style.typo.size = orig_typo_size;
+
+        float x = 0.;
+        style.flags |= FF_FLAG_PRINT_VERTICALLY;
+        for (size_t i = 0; i < 10; ++i) {
+            float w = ff_draw_str8(LOREM, sizeof(LOREM) - 1, x, y,
+                                   (float *)projection, style)
+                          .width;
+            x += w + 10;
+            style.typo.size += 2.;
+        }
+        style.flags &= ~FF_FLAG_PRINT_VERTICALLY;
+        style.typo.size = orig_typo_size;
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-    ff_glyph_vec_destroy(&glyphs);
 
     ff_terminate();
     destroy_gl_ctx();
